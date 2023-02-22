f8a772e90ae126349b59138a729b2d24bc9318e2
Tevž Basaj <76099047+Tevz-B@users.noreply.github.com>
2023-02-22 06:27:22 +0100

    v0

--- src/compiler/lexer/Lexer.java
+++ src/compiler/lexer/Lexer.java
@@ -59,7 +59,82 @@ public class Lexer {
     // 'bla' je string const
     public List<Symbol> scan() {
         var symbols = new ArrayList<Symbol>();
+    
         // todo: implementacija leksikalne analize
+        System.out.println("Lex analiza todo remove");
+        
+        int line = 1; 
+        int startLine = 1;
+        int column = 1;
+        int startColumn = 1;
+        String lexeme = "";
+        for (int sourcePos = 0; sourcePos < source.length(); sourcePos++) {
+            char c = source.charAt(sourcePos);
+            if (c > 32 && c < 127) {
+                if (c == 32) { // space
+                    // zamakni za 1
+                    
+                    if (!lexeme.isEmpty()) {
+                        // insert symbol
+                        TokenType type = keywordMapping.get(lexeme);
+                        if (type == null) {
+                            // todo
+                            type = IDENTIFIER;
+                        }
+                        symbols.add(new Symbol(new Location(startLine, startColumn), new Location(line, column), type, lexeme)); // maybe copy string
+                        lexeme = "";
+                    }
+                    ++column;
+                    // reset startLine and column count
+                    startLine = line;
+                    startColumn = column;
+                    lexeme = "";
+                }
+                else if (c == 9) { // tab
+                    // zamakni za 4
+
+                    if (!lexeme.isEmpty()) {
+                        // insert symbol
+                        TokenType type = keywordMapping.get(lexeme);
+                        if (type == null) {
+                            // todo
+                            type = IDENTIFIER;
+                        }
+                        symbols.add(new Symbol(new Location(startLine, startColumn), new Location(line, column), type, lexeme)); // maybe copy string
+                        lexeme = "";
+
+                    }
+                    column += 4;
+
+                    startLine = line;
+                    startColumn = column;
+                }
+                else if (c == 10 || c == 13) { // newline
+                    if (!lexeme.isEmpty()) {
+                        // insert symbol
+                        TokenType type = keywordMapping.get(lexeme);
+                        if (type == null) {
+                            // todo
+                            type = IDENTIFIER;
+                        }
+                        symbols.add(new Symbol(new Location(startLine, startColumn), new Location(line, column), type, lexeme)); // maybe copy string
+                        lexeme = "";
+
+                    }
+                    column = 1;
+                    ++line;
+
+                    startLine = line;
+                    startColumn = column;
+           
+                }
+                else if (c > 64 && c < 91 || c > 96 && c < 123 )  { // crka
+                    ++column;
+                    lexeme = lexeme + c;
+                }
+            }
+        }
+
         return symbols;
     }
 }
